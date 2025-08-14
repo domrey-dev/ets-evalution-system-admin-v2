4efbfe7e86096053e9a144663a462f36cd70f9a8
reaksa-maii <maireaska@gmail.com>
2025-08-15 01:56:06 +0700

    upate script build docker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
-chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+chown -R $USER:www-data storage bootstrap/cache
+chmod -R 775 storage bootstrap/cache
 
 exec "$@"
